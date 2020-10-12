a75efd5e2f64ad4d95c855ff639047d508877e9f
heejra <theheejra@gmail.com>
2020-10-12 22:14:41 +0700

    device emulation set up

--- tests/example.test.js
+++ tests/example.test.js
@@ -363,20 +363,132 @@
 
 // Keyboard press simulation
 
+// const puppeteer = require('puppeteer')
+// const expect = require('chai').expect
+
+// describe('First Puppeteer Test', () => {
+//     it('should launch the browser', async function () {
+//         const browser = await puppeteer.launch({
+//             headless: false,
+//             slowMo: 10,
+//             devtools: false
+//         })
+//         const page = await browser.newPage()
+//         await page.setDefaultTimeout(10000)
+//         await page.setDefaultNavigationTimeout(20000)
+//         await page.goto('http://example.com')
+//         await page.waitForXPath('//h1')
+//         const title = await page.title()
+//         const url = await page.url()
+//         const text = await page.$eval('h1', element => element.textContent)
+//         const count = await page.$$eval('p', element => element.length)
+
+//         expect(title).to.be.a('string', 'Example Domain')
+//         expect(url).to.include('example.com')
+//         expect(text).to.be.a('string', 'Example Domain')
+//         expect(count).to.equal(2)
+
+//         await page.goto('http://zero.webappsecurity.com')
+//         await page.waitForSelector('#searchTerm')
+//         await page.type('#searchTerm', 'Transfer')
+//         await page.keyboard.press('Enter', {
+//             delay: 10
+//         })
+//         await page.waitFor(5000)
+
+//         await browser.close()
+
+//     })
+// })
+//  ==================================================================================
+
+
+// Element Not Exist
+
+// const puppeteer = require('puppeteer')
+// const expect = require('chai').expect
+
+// describe('First Puppeteer Test', () => {
+//     it('should launch the browser', async function () {
+//         const browser = await puppeteer.launch({
+//             headless: false,
+//             slowMo: 10,
+//             devtools: false
+//         })
+//         const page = await browser.newPage()
+//         await page.setDefaultTimeout(10000)
+//         await page.setDefaultNavigationTimeout(20000)
+//         await page.goto('http://example.com')
+//         await page.waitForXPath('//h1')
+//         const title = await page.title()
+//         const url = await page.url()
+//         const text = await page.$eval('h1', element => element.textContent)
+//         const count = await page.$$eval('p', element => element.length)
+
+//         expect(title).to.be.a('string', 'Example Domain')
+//         expect(url).to.include('example.com')
+//         expect(text).to.be.a('string', 'Example Domain')
+//         expect(count).to.equal(2)
+
+//         await page.goto('http://zero.webappsecurity.com')
+//         await page.waitForSelector('#signin_button')
+//         await page.click('#signin_button')
+//         await page.waitFor(() => !document.querySelector('#signin_button'))
+//         await page.waitForSelector('#signin_button', {
+//             hidden: true,
+//             timeout: 3000,
+//         })
+
+//         await browser.close()
+
+//     })
+// })
+//  ==================================================================================
+
+
+// Test Hooks
+
 const puppeteer = require('puppeteer')
 const expect = require('chai').expect
 
 describe('First Puppeteer Test', () => {
-    it('should launch the browser', async function () {
-        const browser = await puppeteer.launch({
+    let browser
+    let page
+
+    before(async function () {
+        browser = await puppeteer.launch({
             headless: false,
             slowMo: 10,
-            devtools: false
+            devtools: false,
         })
-        const page = await browser.newPage()
+        page = await browser.newPage()
         await page.setDefaultTimeout(10000)
         await page.setDefaultNavigationTimeout(20000)
+    })
+    after(async function () {
+        await browser.close()
+    })
+
+
+    beforeEach(async function () {
+        //  run before each test step
+    })
+
+    afterEach(async function () {
+        //  run after each test step
+    })
+
+    it('should launch the browser', async function () {
+        // const browser = await puppeteer.launch({
+        //     headless: false,
+        //     slowMo: 10,
+        //     devtools: false
+        // })
+        // const page = await browser.newPage()
+        // await page.setDefaultTimeout(10000)
+        // await page.setDefaultNavigationTimeout(20000)
         await page.goto('http://example.com')
+        await page.waitForXPath('//h1')
         const title = await page.title()
         const url = await page.url()
         const text = await page.$eval('h1', element => element.textContent)
@@ -388,14 +500,15 @@ describe('First Puppeteer Test', () => {
         expect(count).to.equal(2)
 
         await page.goto('http://zero.webappsecurity.com')
-        await page.waitForSelector('#searchTerm')
-        await page.type('#searchTerm', 'Transfer')
-        await page.keyboard.press('Enter', {
-            delay: 10
+        await page.waitForSelector('#signin_button')
+        await page.click('#signin_button')
+        await page.waitFor(() => !document.querySelector('#signin_button'))
+        await page.waitForSelector('#signin_button', {
+            hidden: true,
+            timeout: 3000,
         })
-        await page.waitFor(5000)
 
-        await browser.close()
+        // await browser.close()
 
     })
 })
